481dbe74e9b470ec8e3f2b23c40ad24da1b99b59
Samar bel hajj amor <belhajjamorsamar@gmail.com>
2024-12-16 00:29:57 +0100

    add contact page

--- my-app/app/contact/page.jsx
+++ my-app/app/contact/page.jsx
@@ -1,37 +1,94 @@
 "use client"
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Select, SelectContent , SelectGroup, SelectItem, SelectLabel , SelectTrigger,SelectValue } from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { FaMapMarkedAlt, FaPhone , FaEnvelope} from "react-icons/fa";
 import {motion} from "framer-motion";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/components/ui/tabs';
 
+const  info =[{
+  icon:<FaPhone/>,
+  title: "phone",
+  description : "(+216) 55 114 715"
+ },
+ {
+  icon:<FaEnvelope/>,
+  title: "Email",
+  description : "belhajjamorsamar@gmail.com"
+ },
+ {
+  icon:<FaMapMarkedAlt/>,
+  title: "Adress",
+  description : "Tunisia Nabeul Haouria"
+ }]
+
+
 const page = () => {
   return (
-    <motion.div
+    <motion.section
     initial={{ opacity: 0 }}
     animate={{
       opacity: 1,
       transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
     }}
-  className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
-  >
-    <div className="container mx-auto">
-     <Tabs defaultValue="experience"
-      className="flex flex-col xl:flex-row gap-[60px]">
-     <TabsList>
-       <TabsTrigger>Experience</TabsTrigger>
-       <TabsTrigger>Education</TabsTrigger>
-       <TabsTrigger>Skills</TabsTrigger>
-       <TabsTrigger>About me</TabsTrigger>
-     </TabsList>
+  className="py-6">
+    <div className="container mx-auto ">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+            <div className="xl:h-[54%] order-2 xl:order-none ">
+              <form className="flex flex-col gap-6 p-10 bg-[#2727272c] rounded-xl">
+              <h3 className="text-4xl text-accent ">Let's work together</h3>
+              <p className="text-white/60">hi i'm samar if you have any idae and you should tow ork toghter i'm here to help you</p>
+              
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 focus:border-accent">
+                <Input type="firstname" placeholder="Firstname"/>
+                <Input type="lastname" placeholder="Lastname"/>
+                <Input type="email" placeholder="Email address"/>
+                <Input type="phone" placeholder="Phone number"/>
+              </div>
 
-     {/* content*/}
-     <div className="min-h[70vh] w-full "></div>
-     {/* Experience*/}
-     <TabsContent  value ="experience" className=" w-full ">
-     experience
-
-     </TabsContent>
-     </Tabs>
+              {/* Select*/}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service"/>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">Data Scientist</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* textarea */}
+              <Textarea className="h-[200px]" placeholder="type your message here"
+              />
+              {/* btn */}
+              <Button size="md" className="max-w-40 h-10">Send message</Button>
+              </form>
+            </div>
+           {/* info */}
+            <div className="flex-1 flex items-center xl:justify-end order-1 x:order-none mb-8 xl:mb-0 ">
+            <ul className="flex flex-col gap-10">
+              {info.map((item,index)=>{
+                return(
+                 <li key={index} className="flex items-center gap-6">
+                 <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent  rounded-md  flex items-center justify-center">
+                  <div className="text-[28px]">{item.icon}</div>
+                 </div>
+                 <div className="flex-1">
+                 <p className="text-white/60">{item.title}</p>
+                 <h3 className="text-xl">{item.description}</h3>
+                 </div>
+                 </li>);
+              })}
+            </ul>
+            </div>
+        </div>
     </div>
-  </motion.div>
+  </motion.section>
 );
 }
 
